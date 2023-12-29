6cb14c0b7c6a3247cb5245ea0122ead6084b017e
muhsinachipra <muhsinachipra@gmail.com>
2023-12-29 11:19:37 +0530

    final 3

--- index.js
+++ index.js
@@ -4,22 +4,25 @@ const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
 
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGO_URL)
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
 
-const path= require("path")
-app.use('/car',express.static(path.join(__dirname,'public')));
+const path = require("path")
+app.use('/car', express.static(path.join(__dirname, 'public')));
 
-const userRoute=require("./routes/userRoute")
-app.use('/',userRoute)
+const userRoute = require("./routes/userRoute")
+app.use('/', userRoute)
 
-const adminRoute= require("./routes/adminRoute")
-app.use('/admin',adminRoute)
+const adminRoute = require("./routes/adminRoute")
+app.use('/admin', adminRoute)
 
+app.use('*', (req, res) => {
+    res.status(404).render(__dirname + '/views/users/404.ejs')
+})
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log("......SERVER STARTED......");
 })
